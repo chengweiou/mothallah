4b6dab205cf36b0de8ca7dc8a83fdb6440cfa01a
chengweiou <chengwei.ou.dora@gmail.com>
2021-06-04 11:06:39 -0700

    docker compose upgrade

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ chmod +x uat.sh
 ### url example
 /proj/upload/pet/avatar.gif
 http://127.0.0.1:60004/upload/pet/avatar.gif
+
+##### 单独启动容器
+###### depend on address in triangulum, work with triangulum
+```
+docker network net
+docker run --rm -it -d --name mothallah -p 60004:80 --network net -v ~/Desktop/docker/universe/triangulum/upload/:/proj/upload/ -v ~/Desktop/docker/universe/mothallah/default.conf:/etc/nginx/conf.d/default.conf nginx
+```
--- uat.sh
+++ uat.sh
@@ -1,6 +1,8 @@
 cp nginx.conf ~/Desktop/docker/universe/mothallah/default.conf
+cp docker-compose.yml ~/Desktop/docker/universe/mothallah/docker-compose.yml
 cd ~/Desktop/docker/universe/mothallah
-docker stop mothallah
-# depend on address in triangulum
-docker run --rm -it -d --name mothallah -p 60004:80 --network net -v ~/Desktop/docker/universe/triangulum/upload/:/proj/upload/ -v ~/Desktop/docker/universe/mothallah/default.conf:/etc/nginx/conf.d/default.conf nginx
+
+docker compose down
+docker compose rm -f
+docker compose up -d
 
